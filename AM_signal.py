64897513793dc5b029ed14641e5530da09721af2
KingYau <kingyau991319@yahoo.com.hk>
2020-02-24 13:35:35 +0800

    Update AM_signal.py

--- AM_signal.py
+++ AM_signal.py
@@ -1,11 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def DSBLC_singal(amp=5, modulation_index=1, freq_of_carrier_signal=1200, freq_of_message_signal=300,size = 1000):
 
-def DSBLC_singal(amp=5, modulation_index=1, freq_of_carrier_signal=1200, freq_of_message_signal=300):
-    X = np.linspace(-5, 5, 1000)
-    formula = 2 * np.cos(1500 * np.pi * X) + 2 * \
-        np.cos(900 * np.pi * X) + 7 * np.cos(1200 * np.pi * X)
+    X = np.linspace(-5, 5, size)
+    formula = 2 * np.cos((freq_of_message_signal+freq_of_carrier_signal) * np.pi * X) + 2 * \
+        np.cos((freq_of_carrier_signal-freq_of_message_signal) * np.pi * X) + 7 * np.cos(freq_of_carrier_signal * np.pi * X)
+
+    # if add AWGN
+    mu , sigma = 0, 0.1
+    s = np.random.normal(mu, sigma,size)
+    formula += s
 
     plt.plot(X, formula, color="red", alpha=0.5)
 
